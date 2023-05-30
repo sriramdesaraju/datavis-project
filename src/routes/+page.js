85c5740d96257926a8ef8be33d337f3ea23088b6
sriramdesaraju <sriram.desaraju@live.com>
2023-05-30 02:15:31 +0200

    remove server hooks and add load data

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,25 +1,13 @@
 export const load = ({ fetch }) => {
   const fetchCars = async () => {
     const res = await fetch(
-      'https://vda-lab.github.io/assets/vast2021_gps_coordinates.json',
-      {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-      }
+      'http://localhost:5173/vast2021_gps_coordinates.json'
     );
     const data = await res.json();
     return data;
   };
   const fetchPOI = async () => {
-    const res = await fetch(
-      'https://vda-lab.github.io/assets/vast2021_businesses.json',
-      {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-      }
-    );
+    const res = await fetch('http://localhost:5173/vast2021_businesses.json');
     const data = await res.json();
     return data;
   };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   export let data
+  console.log(data)
 </script>
 
 <h1> Sriram Desaraju - UHasselt - 2262335</h1>
 <h2> Overview</h2>
-console.log(data)
